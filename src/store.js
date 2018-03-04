490bfd3f444e494c1f7a20374dd9caa176638e16
Jehan <jehan.tremback@gmail.com>
2018-03-04 15:08:59 -0800

    demonstrate different actions syntax - change code to use different syntax - demonstrate action that takes an argument and modifies state several times

--- src/store.js
+++ src/store.js
@@ -1,21 +1,28 @@
-import { initStore } from 'react-stateful'
+import { initStore } from "./react-stateful.js";
 import { devTool } from './devtool'
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 const store = {
   initialState: {
     count: 10,
-    user: null,
+    user: null
   },
   actions: {
-    increment: ({ count }) => ({ count: count + 1 }),
-    getUser: async () => {
-      const response = await fetch('https://api.github.com/users/didierfranc')
+    increment: ({ state: { count } }) => ({ count: count + 1 }),
+    getUser: async ({ setState }, user) => {
+      setState({ loading: true })
+      const response = await fetch('https://api.github.com/users/' + user)
+      await timeout(1000)
       const body = await response.json()
       return {
-        user: { avatar: body.avatar_url },
+        loading: false,
+        user: { avatar: body.avatar_url }
       }
-    },
-  },
+    }
+  }
 }
 
 export const {
